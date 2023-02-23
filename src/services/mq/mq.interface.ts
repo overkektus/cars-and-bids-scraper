81ca6fad3211009dc02b0232eacf21e4429b788e
Yahor Piskunou <egor.piskunou@gmail.com>
2023-02-23 20:50:21 +0100

    update mq interface

--- src/services/mq/mq.interface.ts
+++ src/services/mq/mq.interface.ts
@@ -2,6 +2,7 @@ export interface IMQ<C> {
   connect(): Promise<void>;
   closeConnection(): Promise<void>;
   sendData<T>(queueName: string, data: T): void;
+  accept(data: C): void;
   setConsume(queueName: string, consumer: (msg: C) => void): void;
   assertQueue(queueName: string): void;
 }
