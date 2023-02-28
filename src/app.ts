79132efee220035820a64e9d8d131b1b0075c5d3
Yahor Piskunou <egor.piskunou@gmail.com>
2023-02-28 19:24:52 +0100

    update app file
    - added new rabbitMQ consumer
    - added db connection
    - added assertQueue in rabbitMQ

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,42 @@
 import { inject, injectable } from "inversify";
+import mongoose from "mongoose";
 
 import { IApp } from "./app.interface";
+import { INITIAL_QUEUE_NAME, CHECK_QUEUE_NAME } from "./constants";
+import { IConfigService } from "./services/config/config.interface";
 import { IMQ } from "./services/mq/mq.interface";
 import { ConsumerMessageType } from "./services/mq/rabbitMQ";
 import { IParser } from "./services/parser/parser.interface";
 import { TYPES } from "./types";
 
-const QUEUE_NAME = "initial";
-
 @injectable()
 export class App implements IApp {
   constructor(
+    @inject(TYPES.Config) public config: IConfigService,
     @inject(TYPES.RabbitMQ) public rabbitMQ: IMQ<ConsumerMessageType>,
     @inject(TYPES.Parser) public parser: IParser
   ) { }
 
   async launch(): Promise<void> {
-    await this.rabbitMQ.connect();
-    this.rabbitMQ.assertQueue(QUEUE_NAME);
+    await mongoose.set('strictQuery', true);
+    await mongoose.connect(this.config.get("MONGODB_URI"));
+    console.log('db connected');
+    
+    await this.rabbitMQ.connect(this.config.get("RABBITMQ_URI"));
+    this.rabbitMQ.assertQueue(INITIAL_QUEUE_NAME);
+    this.rabbitMQ.assertQueue(CHECK_QUEUE_NAME);
 
-    await this.parser.launchParser();
-
-    this.rabbitMQ.setConsume(QUEUE_NAME, (data) => {
+    this.rabbitMQ.setConsume(INITIAL_QUEUE_NAME, (data) => {
       console.log("Data received : ", `${Buffer.from(data!.content)}`);
 
-      const url: string = Buffer.from(data!.content).toString();
+      const url: string = Buffer.from(data!.content).toString().slice(1, -1);
       this.parser.initialConsumer(url);
+      this.rabbitMQ.accept(data);
+    });
+
+    this.rabbitMQ.setConsume(CHECK_QUEUE_NAME, (data) => {
+      console.log(Buffer.from(data!.content).toString());
+      this.parser.checkConsumer(Buffer.from(data!.content).toString().slice(1, -1));
       this.rabbitMQ.accept(data);
     });
   }
